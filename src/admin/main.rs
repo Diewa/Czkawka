6f3bce07a0a8f712b4e5f5ad2179fb401292b0ad
Dawid <urbanowsky.d@gmail.com>
2023-11-30 20:32:00 +0100

    Mimi created topic

--- src/admin/main.rs
+++ src/admin/main.rs
@@ -1,29 +1,22 @@
 use rocket::fs::{FileServer, relative};
-use crate::topic::TopicService;
 
 #[macro_use] extern crate rocket;
 
 mod api;
 mod storage;
-mod topic;
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .configure(rocket::Config::figment().merge(("port", 8081)))
-
-        // PUBLISH
         .mount("/publish", routes![
             api::receiver::publish_message,
             api::receiver::get_offset
         ])
 
-        // ADMIN
         .mount("/admin", routes![
-            api::admin::get_topics,
             api::admin::create_topic
         ])
-        .manage(TopicService::new())
         
-        .mount("/", FileServer::from(relative!("src/broker/web")))
+        .mount("/", FileServer::from(relative!("web")))
 }
--- src/broker/api/admin.rs
+++ src/broker/api/admin.rs
@@ -1,4 +1,53 @@
-#[get("/create")]
-pub fn create_topic() -> &'static str {
-    "<h1>OmG new topic!!1</h1>"
+use rocket::State;
+
+use serde::Deserialize;
+use rocket::serde::json::Json;
+
+use crate::topic::topic_service::{TopicService, TopicEntry};
+
+#[derive(Deserialize)]
+pub struct TopicDTO {
+    name: String,
+    owner: String
 }
+
+
+/*
+#[post("/articles", format = "json", data = "<new_article>")]
+pub async fn post_articles(
+    auth: Auth,
+    new_article: Json<NewArticle>,
+    db: Db,
+)
+*/
+
+#[post("/topics")]
+pub fn create_topic(
+    new_topic: Json<TopicDTO>, 
+    topicService: &State<TopicService>
+) -> &'static str {
+
+    let topic_entry = TopicEntry {
+        name: new_topic.name,
+        owner: new_topic.owner
+    };
+
+    topicService.create_topic(name.clone(), topic_entry);
+
+    "OK!"
+}
+
+#[get("/topics")]
+pub fn get_topics(topicService: &State<TopicService>) -> &'static str {
+    "T1, T2, T3"
+}
+
+/*
+// rustc --version
+// rustup update
+
+
+
+
+
+*/
